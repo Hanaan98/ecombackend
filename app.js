a5a65297984a77078e36bb4060a3fa5c4cf6a47b
Hanaan98 <bcsf20m502@pucit.edu.pk>
2025-05-06 01:30:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const multer = require('multer');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+require("dotenv").config();
+app.use(cors());
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/send-email', upload.single('payslip'), async (req, res) => {
+  const { name, email } = req.body;
+  const payslipPdf = req.file;
+
+  if (!email || !name || !payslipPdf) {
+    return res.status(400).send("Missing required data");
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.dreamhost.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.DREAMHOST_EMAIL,
+      pass: process.env.DREAMHOST_PASS
+    }
+  });
+
+  try {
+    await transporter.sendMail({
+      from: '"Ecommercesteem" <asad@ecommercesteem.com>',
+      to: email,
+      subject: `Your Payslip`,
+      text: `Hi ${name},\n\nPlease find your salary slip attached.`,
+      attachments: [
+        {
+          filename: `Payslip-${name}.pdf`,
+          content: payslipPdf.buffer,
+          contentType: 'application/pdf'
+        }
+      ]
+    });
+
+    res.send("Email sent successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to send email");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
