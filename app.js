23ab6d9a955d7fc482b27653a5ef3d25c89a0025
Hanaan98 <bcsf20m502@pucit.edu.pk>
2025-05-06 03:36:06 +0500

    Signature Added

--- app.js
+++ app.js
@@ -33,12 +33,44 @@ app.post('/send-email', upload.single('payslip'), async (req, res) => {
       from: `"Ecommercesteem" <${process.env.DREAMHOST_EMAIL}>`,
       to: email,
       subject: `Your Payslip`,
-      text: `Hi ${name},\n\nPlease find your salary slip attached.`,
+      html: `
+        <div style="font-family: Arial, sans-serif; color: #000;">
+          <p>Hi ${name},</p>
+          <p>Please find your salary slip attached.</p>
+
+          <br/>
+          <p>Best regards,<br/>Asad Niaz</p>
+
+          <div style="margin-top: 10px;">
+            <img src="cid:logo" alt="Ecommercesteem Logo" style="width: 100px;"/>
+          </div>
+
+          <div style="font-size: 14px; margin-top: 10px;">
+            <strong>Ecommerce Steem</strong><br/>
+            <a href="https://calendly.com/contact-ecommercesteem" style="color: #0066cc; text-decoration: none;">Schedule a Meeting</a><br/>
+            🌐 Website: <a href="https://ecommercesteem.com" style="color: #0066cc;">ecommercesteem.com</a><br/>
+            📧 Email: <a href="mailto:asad@ecommercesteem.com" style="color: #0066cc;">asad@ecommercesteem.com</a><br/>
+            📞 Phone: +44 7915391870
+          </div>
+
+          <div style="margin-top: 10px;">
+            <p>Stay connected:</p>
+            <a href="https://www.facebook.com/ecommercesteem0><img src="https://cdn-icons-png.flaticon.com/24/145/145802.png" alt="Facebook" /></a>
+            <a href="https://www.instagram.com/ecommercesteem/"><img src="https://cdn-icons-png.flaticon.com/24/2111/2111463.png" alt="Instagram" /></a>
+            <a href="https://www.linkedin.com/company/ecommercesteem-ltd/"><img src="https://cdn-icons-png.flaticon.com/24/145/145807.png" alt="LinkedIn" /></a>
+          </div>
+        </div>
+      `,
       attachments: [
         {
           filename: `Payslip-${name}.pdf`,
           content: payslipPdf.buffer,
           contentType: 'application/pdf'
+        },
+        {
+          filename: 'logo.png',
+          path: __dirname + '/public/logo.png', 
+          cid: 'logo'
         }
       ]
     });
